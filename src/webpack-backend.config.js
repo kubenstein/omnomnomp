9becb9d2fe7e93ede00154edea143c462bfa4763
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-02 15:40:30 +0900

    add support for async/await

--- src/webpack-backend.config.js
+++ src/webpack-backend.config.js
@@ -36,7 +36,14 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
+              presets: [
+                ['@babel/preset-env', {
+                  targets: {
+                    node: 'current',
+                  },
+                }],
+                '@babel/preset-react',
+              ],
               plugins: [
                 ['@babel/plugin-proposal-class-properties', { loose: true }],
                 '@babel/plugin-proposal-object-rest-spread',
